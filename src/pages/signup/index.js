8a626c6c954a8f08a9c252b5b9dca5504ab03240
Lais Calderaro <laiscalderaro06@gmail.com>
2024-05-30 14:57:13 -0300

    Edit Placeholder

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -61,7 +61,7 @@ const SignUpPage = () => {
                   name="first_name"
                   id="first_name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
+                  placeholder="Primeiro nome"
                   required=""
                   onChange={(e) => setFirstname(e.target.value)}
                 />
@@ -73,7 +73,7 @@ const SignUpPage = () => {
                   name="last_name"
                   id="last_name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
+                  placeholder="Ultimo nome"
                   required=""
                   onChange={(e) => setLastname(e.target.value)}
                 />
